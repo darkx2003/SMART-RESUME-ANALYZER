2ae0bd803b524e05fad26a43691a4c6ceacc6794
Pawan Sharma <136601439+darkx2003@users.noreply.github.com>
2024-08-16 12:11:04 +0530

    Add files via upload

---
+++ skill.py
@@ -0,0 +1,22 @@
+# Predefined skills for different fields
+
+ds_skills = [
+    "python", "r", "sql", "machine learning", "data analysis", "statistics", "data mining", "deep learning",
+    "natural language processing", "tensorflow", "keras", "pandas", "numpy", "matplotlib", "scikit-learn"
+]
+
+web_skills = [
+    "html", "css", "javascript", "react", "angular", "node.js", "express", "mongodb", "sql", "bootstrap", "django", "flask"
+]
+
+android_skills = [
+    "java", "kotlin", "android sdk", "android studio", "xml", "gradle", "firebase", "retrofit", "sqlite", "mvvm", "jetpack"
+]
+
+ios_skills = [
+    "swift", "objective-c", "xcode", "cocoa touch", "core data", "ios sdk", "storyboards", "auto layout", "uikit", "alamofire"
+]
+
+uiux_skills = [
+    "figma", "adobe xd", "sketch", "wireframing", "prototyping", "user research", "interaction design", "visual design", "usability testing"
+]
